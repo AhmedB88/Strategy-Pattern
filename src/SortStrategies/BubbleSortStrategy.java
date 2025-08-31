f2214b59374961e0ce1bc653d39aa34a5b4e6dde
Ahmad Bako <ahmedbako999@gmail.com>
2025-08-31 20:49:45 +0200

    Strategy-Pattern

---
+++ src/SortStrategies/BubbleSortStrategy.java
@@ -0,0 +1,25 @@
+package SortStrategies;
+
+// BubbleSort
+public class BubbleSortStrategy implements SortStrategy {
+
+    @Override
+    public long sort(int[] numbers) {
+        long startTime = System.nanoTime(); // Startzeit
+
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = 0; j < numbers.length - i - 1; j++) {
+                if (numbers[j] > numbers[j + 1]) {
+                    int temp = numbers[j];
+                    numbers[j] = numbers[j + 1];
+                    numbers[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println("BubbleSort: " + java.util.Arrays.toString(numbers));
+        long endTime = System.nanoTime();    // Endzeit
+        long duration = endTime - startTime; // Dauer in Nanosekunden
+        System.out.println("Dauer: " + duration + " ns");
+        return duration;
+    }
+}
---
+++ src/SortStrategies/SelectionSortStrategy.java
@@ -0,0 +1,27 @@
+package SortStrategies;
+
+public class SelectionSortStrategy implements SortStrategy {
+    @Override
+    public long sort(int[] numbers) {
+
+        long startTime = System.nanoTime(); // Startzeit
+
+        int n = numbers.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIdx = i;
+            for (int j = i + 1; j < n; j++) {
+                if (numbers[j] < numbers[minIdx]) minIdx = j;
+            }
+            int temp = numbers[minIdx];
+            numbers[minIdx] = numbers[i];
+            numbers[i] = temp;
+        }
+        System.out.println("SelectionSort: " + java.util.Arrays.toString(numbers));
+        long endTime = System.nanoTime();    // Endzeit
+
+        long duration = endTime - startTime; // Dauer in Nanosekunden
+        System.out.println("Dauer: " + duration + " ns");
+        return duration;
+
+    }
+}
